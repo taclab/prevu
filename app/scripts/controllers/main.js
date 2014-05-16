9cf9ab564e0becce3e6843d4f0756919bcf270c1
taclab <mehdi.bourgeois@gmail.com>
2014-05-16 13:26:11 +0200

    ajout des couverture sur Main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,33 @@
 
 angular.module('prevuApp')
   .controller('MainCtrl', function ($scope, prevuAPIservice) {
-	$scope.clock="eaze";
 
-	function topBooks () {
-		prevuAPIservice.getTopBooks().success(function (response) {
-			console.log(response);
-			$scope.topBooks = response;
-    	});
-	}
+	prevuAPIservice.getTopBooks().success(function (responseBook) {
+        // Ajout des metas données Amazon ResponseBook
+        angular.forEach(responseBook.books, function(book){
+            // GET AMAZON
+            prevuAPIservice.getCoverBook(book.biblionumber).success(function (responseCover) {
+                book.TinyImage = responseCover.TinyImage;
+                book.LargeImage = responseCover.LargeImage;
+                book.MediumImage = responseCover.MediumImage;
+                book.Edito = responseCover.Edito;
+            });
+        });
+        $scope.topBooks = responseBook;
+    });
+    
+    // // Récupération des meta donnes Amazon
+    // function getCover (array) {
+    //     // Faire une requete sql en tableau
+    //     prevuAPIservice.getCoverBook(array).success(function (responseCover) {
+    //         console.log(responseCover);
+    //         return responseCover;
+    //     });
+    // }
+
+
+    //unescape(string)
+
 
     // $scope.topBooks = function() {
     // 	console.log("te");
@@ -22,7 +41,7 @@ angular.module('prevuApp')
     
 
 
-    topBooks();
+    //topBooks();
 
 
   });
--- app/scripts/services/prevuapiservice.js
+++ app/scripts/services/prevuapiservice.js
@@ -3,36 +3,42 @@
 angular.module('prevuApp')
   .factory('prevuAPIservice', function ($http) {
     var prevuAPI = {};
+    var prevuAPI_url = "http://localhost:8888/prevu/application/"
 
     prevuAPI.searchAuthor =  function(author) {
       return $http({
-        url : 'http://localhost:8888/prevu/application/api/author/search/'+author
+        url : prevuAPI_url+'api/author/search/'+author
       });
     };
 
     prevuAPI.searchBook =  function(title) {
       return $http({
-        url : 'http://localhost:8888/prevu/application/api/book/search/'+title
+        url : prevuAPI_url+'api/book/search/'+title
       });
     };
 
     prevuAPI.searchBookByAuthor =  function(author) {
       return $http({
-        url : 'http://localhost:8888/prevu/application/api/search/'+author
+        url : prevuAPI_url+'api/search/'+author
       });
     };
 
-
-
     prevuAPI.searchIssuesByTitle =  function(title) {
       return $http({
-        url : 'http://localhost:8888/prevu/application/api/issues/title/'+title
+        url : prevuAPI_url+'api/issues/title/'+title
       });
     };
     
     prevuAPI.getTopBooks =  function() {
       return $http({
-        url : 'http://localhost:8888/prevu/application/api/books/top/'
+        url : prevuAPI_url+'api/books/top/'
+      });
+    };
+
+    // GET COVER IMAGE
+    prevuAPI.getCoverBook =  function(id) {
+      return $http({
+        url : prevuAPI_url+'api/book/cover/'+id
       });
     };
 
