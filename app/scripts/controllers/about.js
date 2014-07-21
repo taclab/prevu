0d31167ab17261c7d7e3e30eed130f5b030d1153
taclab <mehdi.bourgeois@gmail.com>
2014-07-21 16:44:40 +0200

    Ajout de la page About

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,10 +1,7 @@
 'use strict';
 
 angular.module('prevuApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function ($scope, $rootScope) {
+  $rootScope.bodyClass = "viewAbout";
+
   });
