07d2652d54f15e555badbe25ee555306ebe8f59f
taclab <mehdi.bourgeois@gmail.Com>
2016-04-06 11:57:14 +0200

    Compilation pour la v2

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,10 +1,5 @@
-angular.module("config", [])
+angular.module('config', [])
 
-.constant("ENV", {
-  "name": "development",
-  //"apiEndpoint": "http://localhost:8888/prevu/application-ouerdia/"
-  "apiEndpoint": "http://prevu.labex-arts-h2h.fr/"
-  
-})
+.constant('ENV', {name:'development',apiEndpoint:'http://localhost:8888/prevu/application/'})
 
 ;
